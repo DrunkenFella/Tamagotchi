5f2b0cf7e5dd887fa66370a438351d9779456f62
Drunken Fella <alex.ostlund@elev.ga.ntig.se>
2023-10-06 10:21:11 +0200

    Start

---
+++ tamagotchi/Tamagotchi.cs
@@ -0,0 +1,57 @@
+namespace Namespace;
+public class Class
+{
+
+    private int hunger;
+    private int boredom;
+
+    
+    private List<string> words
+    {
+        
+    }
+    private bool isAlive
+    {
+        
+    }
+    private Random generator
+    {
+
+    }
+    public List<string> name
+    {
+
+    }
+
+
+    public void Feed()
+    {
+
+    }
+    public void Hi()
+    {
+
+    }
+    public void Teach(word: string)
+    {
+
+    }
+    public void Tick()
+    {
+        
+    }
+    public void PrintStats()
+    {
+
+    }
+    public bool GetAlive()
+    {
+
+    }
+    private void ReduceBoredom()
+    {
+
+    }
+
+
+}
